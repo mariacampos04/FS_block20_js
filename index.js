320fe3d9ca0dcfb6383ae0b3c1c736248b08473a
Maria Campos <campos.maria04@outlook.com>
2023-05-18 18:02:06 -0500

    for loop

--- index.js
+++ index.js
@@ -12,33 +12,29 @@ const users = [
 function main() {
 //Inside your main() function, grab the "root" element, add an h1 element, 
 //give it text of "FREELANCERS", and append the HTML document.
-const root = document.getElementById("root");
-const h1 = document.createElement("h1");
-h1.innerHTML = "FREELANCERS";
-root.appendChild(h1);
+const rootElement = document.getElementById("root");
+const h1Element = document.createElement("h1");
+h1Element.innerHTML = "FREELANCERS";
+rootElement.appendChild(h1Element);
+
+const unorderedList = document.createElement("ul");
 
 
+for(let i = 0; i < users.length; i++){
+    const user = users[i];
+    const listElement = document.createElement("li"); 
+    listElement.innerHTML = `name: ${user.name} age: ${user.age} occupation: ${user.occupation}`
+    unorderedList.appendChild(listElement);
+
+
+}
+
+rootElement.appendChild(unorderedList);
 //Continue working inside your main() function to create an unordered list element.
 
 
-//ul.setAttribute("id", "myUL")
-
-// for (let i = 0; i<= users.length; i++){
-    
-//     let li = document.createElement("li")
-//     li.innerHTML = `Freelancer name: ${users[i].name}, Age: ${users}`
-//     rootElement.appendChild("li");
-// }
-
 //Loop through the users array creating li elements. 
-// users.forEach(function(item) {
-//     var li = document.createElement("li");
-//     var text = document.createTextNode(item);
-//     li.appendChild(text);
-//     document.getElementById("myUl").appendChild(li);
-//   });
 
-  
 
 //Append those elements to your HTML document.
 
